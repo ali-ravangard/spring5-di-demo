c7213a1217c3a4b6d7ccd94bbf0d4e8d1cb5f137
alirvn18 <alirvngrd@gmail.com>
2021-05-30 17:18:22 +0430

    initial DiDemoApplication Project

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -2,11 +2,13 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DiDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiDemoApplication.class, args);
+
 	}
 }
